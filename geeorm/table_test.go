f01cfe6de547332ed9d80aae183a88fac6700334
MasouShinku <2389837103@qq.com>
2024-02-27 22:19:29 +0800

    增加table的测试用例

--- geeorm/table_test.go
+++ geeorm/table_test.go
@@ -1,7 +1,11 @@
-package session
+package geeorm
 
 import (
+	"fmt"
+	"geeorm/session"
 	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type User struct {
@@ -10,7 +14,9 @@ type User struct {
 }
 
 func TestSession_CreateTable(t *testing.T) {
-	s := NewSession().Model(&User{})
+	engine, _ := NewEngine("sqlite3", "gee.db")
+	defer engine.Close()
+	s := engine.NewSession().Model(&User{})
 	_ = s.DropTable()
 	_ = s.CreateTable()
 	if !s.HasTable() {
@@ -19,9 +25,13 @@ func TestSession_CreateTable(t *testing.T) {
 }
 
 func TestSession_Model(t *testing.T) {
-	s := NewSession().Model(&User{})
+	engine, _ := NewEngine("sqlite3", "gee.db")
+	defer engine.Close()
+	s := engine.NewSession().Model(&User{})
 	table := s.RefTable()
-	s.Model(&Session{})
+	s.Model(&session.Session{})
+	t.Log(fmt.Sprintf("table name is %s", table.Name))
+	t.Log(fmt.Sprintf("refTable name is %s", s.RefTable().Name))
 	if table.Name != "User" || s.RefTable().Name != "Session" {
 		t.Fatal("Failed to change model")
 	}
