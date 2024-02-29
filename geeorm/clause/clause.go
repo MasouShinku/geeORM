e76a2dbd907c1f9614511b876585a263818bb1ff
MasouShinku <2389837103@qq.com>
2024-03-01 00:52:28 +0800

    生成语句规则

--- geeorm/clause/clause.go
+++ geeorm/clause/clause.go
@@ -1,1 +1,49 @@
+// 用于拼接独立子句
 package clause
+
+import (
+	"fmt"
+	"geeorm/log"
+	"strings"
+)
+
+type Type int
+
+const (
+	INSERT Type = iota
+	VALUES
+	SELECT
+	LIMIT
+	WHERE
+	ORDERBY
+)
+
+type Clause struct {
+	sql     map[Type]string
+	sqlVars map[Type][]interface{}
+}
+
+// Set 获取操作类型对应的参数
+func (c *Clause) Set(name Type, vars ...interface{}) {
+	if c.sql == nil {
+		c.sql = make(map[Type]string)
+		c.sqlVars = make(map[Type][]interface{})
+	}
+	sql, vars := generators[name](vars...)
+	c.sql[name] = sql
+	c.sqlVars[name] = vars
+	log.Infoln(fmt.Sprintf("there is vars after Set : [%s] %v", c.sql[name], c.sqlVars[name]))
+}
+
+// Build 生成sql语句
+func (c *Clause) Build(orders ...Type) (string, []interface{}) {
+	var sqls []string
+	var vars []interface{}
+	for _, order := range orders {
+		if sql, ok := c.sql[order]; ok {
+			sqls = append(sqls, sql)
+			vars = append(vars, c.sqlVars[order]...)
+		}
+	}
+	return strings.Join(sqls, " "), vars
+}
--- geeorm/clause/generator.go
+++ geeorm/clause/generator.go
@@ -1,1 +1,78 @@
+// 子句的生成规则
 package clause
+
+import (
+	"fmt"
+	"strings"
+)
+
+type generator func(values ...interface{}) (string, []interface{})
+
+var generators map[Type]generator
+
+func init() {
+	generators = make(map[Type]generator)
+	generators[INSERT] = _insert
+	generators[VALUES] = _values
+	generators[SELECT] = _select
+	generators[LIMIT] = _limit
+	generators[WHERE] = _where
+	generators[ORDERBY] = _orderby
+}
+
+func _insert(values ...interface{}) (string, []interface{}) {
+	// INSERT INTO $tablename ($fields)
+	tablename := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("INSERT INTO %s (%v)", tablename, fields), []interface{}{}
+}
+
+func _values(values ...interface{}) (string, []interface{}) {
+	// VALUES ($v1),($v2),...
+	var bindStr string
+	var sql strings.Builder
+	var vars []interface{}
+	sql.WriteString("VALUES ")
+	for i, value := range values {
+		v := value.([]interface{})
+		if bindStr == "" {
+			var tempVars []string
+			for j := 0; j < len(v); j++ {
+				tempVars = append(tempVars, "?")
+			}
+			bindStr = strings.Join(tempVars, ", ")
+		}
+		sql.WriteString(fmt.Sprintf("(%v)", bindStr))
+		if i+1 != len(values) {
+			sql.WriteString(", ")
+		}
+		vars = append(vars, v...)
+	}
+	return sql.String(), vars
+}
+
+func _select(values ...interface{}) (string, []interface{}) {
+	// SELECT $fields FROM $tablename
+	tablename := values[0]
+	fields := strings.Join(values[1].([]string), ",")
+	return fmt.Sprintf("SELECT %v FROM %s", fields, tablename), []interface{}{}
+	return "", nil
+}
+
+func _limit(values ...interface{}) (string, []interface{}) {
+	// LIMIT $num
+	return fmt.Sprintf("LIMIT ?"), values
+}
+
+func _where(values ...interface{}) (string, []interface{}) {
+	// WHERE $desc
+	desc := values[0]
+	vars := values[1:]
+	return fmt.Sprintf("WHERE %s", desc), vars
+}
+
+func _orderby(values ...interface{}) (string, []interface{}) {
+	// ORDER BY $field
+	field := values[0]
+	return fmt.Sprintf("ORDER BY %s", field), []interface{}{}
+}
