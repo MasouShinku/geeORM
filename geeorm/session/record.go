427a8737c25e126bd9d952dae2c197e079dfc936
MasouShinku <2389837103@qq.com>
2024-03-03 17:01:01 +0800

    添加链式调用及test

--- geeorm/session/record.go
+++ geeorm/session/record.go
@@ -2,6 +2,7 @@
 package session
 
 import (
+	"errors"
 	"geeorm/clause"
 	"reflect"
 )
@@ -68,7 +69,86 @@ func (s *Session) Find(values interface{}) error {
 		}
 		destSlice.Set(reflect.Append(destSlice, dest))
 	}
-
 	return rows.Close()
+}
 
+// Update 更新语句
+// 支持入参1：map[string]any的键值对
+// 支持入参2：kv列表：key1,val1,kay2,val2,...
+func (s *Session) Update(kv ...interface{}) (int64, error) {
+	m, ok := kv[0].(map[string]interface{})
+	// 若不是map形式，则对平铺kv列表进行转换
+	if !ok {
+		m = make(map[string]interface{})
+		for i := 0; i < len(kv); i += 2 {
+			m[kv[i].(string)] = kv[i+1]
+		}
+	}
+
+	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
+	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+// Delete 删除语句
+func (s *Session) Delete() (int64, error) {
+	s.clause.Set(clause.DELETE, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+// Count 计数语句
+func (s *Session) Count() (int64, error) {
+	s.clause.Set(clause.COUNT, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
+	row := s.Raw(sql, vars...).QueryRow()
+	var tmp int64
+	if err := row.Scan(&tmp); err != nil {
+		return 0, err
+	}
+	return tmp, nil
+}
+
+// Limit 限制结果数量
+// 返回Session指针，方便链式调用
+func (s *Session) Limit(num int) *Session {
+	s.clause.Set(clause.LIMIT, num)
+	return s
+}
+
+// Where 设置条件
+// 返回Session指针，方便链式调用
+func (s *Session) Where(desc string, args ...interface{}) *Session {
+	var vars []interface{}
+	s.clause.Set(clause.WHERE, append(append(vars, desc), args...)...)
+	return s
+}
+
+// OrderBy 设置排序
+// 返回Session指针，方便链式调用
+func (s *Session) OrderBy(desc string) *Session {
+	s.clause.Set(clause.ORDERBY, desc)
+	return s
+}
+
+// First 返回第一条记录
+func (s *Session) First(value interface{}) error {
+	desc := reflect.Indirect(reflect.ValueOf(value))
+	descSlice := reflect.New(reflect.SliceOf(desc.Type())).Elem()
+	if err := s.Limit(1).Find(descSlice.Addr().Interface()); err != nil {
+		return err
+	}
+	if descSlice.Len() == 0 {
+		return errors.New("NOT FOUND")
+	}
+	desc.Set(descSlice.Index(0))
+	return nil
 }
